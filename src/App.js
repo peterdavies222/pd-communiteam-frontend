5c5e5682e7e9976fd1e45f677bec7b59d7e7eff3
peterdavies222 <peterdavies222@gmail.com>
2025-05-23 09:40:31 +0800

    apiBase

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ class App {
   constructor(){
     this.name = "Haircuts"
     this.version = "1.0.0"
-    this.apiBase = 'http://localhost:3000'
+    //this.apiBase = 'http://localhost:3000' // for local
+    this.apiBase = 'https://pd-communiteam-backend.onrender.com' // for live
     this.rootEl = document.getElementById("root")
     this.version = "1.0.0"
   }
